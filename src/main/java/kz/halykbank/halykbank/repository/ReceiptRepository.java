b55d6fd66860d6db92f74f32fea22728e62b9b76
Yesnukhanov <yesmukhanov04gmail.com>
2023-11-11 22:30:12 +0600

    repository commit

--- src/main/java/kz/halykbank/halykbank/repository/ReceiptRepository.java
+++ src/main/java/kz/halykbank/halykbank/repository/ReceiptRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Receipt extends JpaRepository<Receipt, Long> {
+public interface ReceiptRepository extends JpaRepository<ReceiptRepository, Long> {
 }
